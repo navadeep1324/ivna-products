63708393cc32747f45c5a838e335c3fe9e585ace
sodham <sodham@kleza.io>
2025-10-28 15:44:38 +0530

    Commit remaining files including clinets assets and temporary files

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,7 +2,11 @@ import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { Features } from "@/components/Features";
 import { Stats } from "@/components/Stats";
+import { ProductShowcase } from "@/components/ProductShowcase";
+import { LeadManagement } from "@/components/LeadManagement";
+import { CallAnalytics } from "@/components/CallAnalytics";
 import { Testimonials } from "@/components/Testimonials";
+import { ContactForm } from "@/components/ContactForm";
 import { CTA } from "@/components/CTA";
 import { Footer } from "@/components/Footer";
 
@@ -11,10 +15,31 @@ const Index = () => {
     <div className="min-h-screen bg-background">
       <Header />
       <main>
+        {/* Introduction Section */}
         <Hero />
+        
+        {/* Features Section */}
         <Features />
+        
+        {/* Lead Management Section */}
+        <LeadManagement />
+        
+        {/* Benefits Section */}
         <Stats />
+        
+        {/* Call Analytics Section */}
+        <CallAnalytics />
+        
+        {/* Product Videos / Screenshots Section */}
+        <ProductShowcase />
+        
+        {/* Testimonials Section */}
         <Testimonials />
+        
+        {/* Contact Form Section */}
+        <ContactForm />
+        
+        {/* Call to Action Section */}
         <CTA />
       </main>
       <Footer />
